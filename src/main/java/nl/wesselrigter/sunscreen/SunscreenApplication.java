39596944f2f5878ecfcf929343996543e1f030ab
rigterw <rigterwessel@gmail.com>
2025-04-29 17:50:33 +0200

    added java project

---
+++ src/main/java/nl/wesselrigter/sunscreen/SunscreenApplication.java
@@ -0,0 +1,13 @@
+package nl.wesselrigter.sunscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunscreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/wesselrigter/sunscreen/SunscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.wesselrigter.sunscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
